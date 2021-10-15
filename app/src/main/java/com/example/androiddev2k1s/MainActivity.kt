54715f269f7ec6c1114c0c414f23dbc4946209cf
capkeik <new.mail2018@mail.ru>
2021-10-15 23:00:57 +0300

    add first task

--- app/src/main/java/com/example/androiddev2k1s/MainActivity.kt
+++ app/src/main/java/com/example/androiddev2k1s/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.androiddev2k1s
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import com.example.androiddev2k1s.databinding.ActivityMainBinding
+
+const val REQUEST_IMAGE_CAPTURE = 1
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.btnEdit.setOnClickListener{
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } else {
+                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val takenImg = data?.extras?.get("data") as Bitmap
+            binding.ivAvatar.setImageBitmap(takenImg)
+            Toast.makeText(this, "Edited successfully", Toast.LENGTH_SHORT).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
