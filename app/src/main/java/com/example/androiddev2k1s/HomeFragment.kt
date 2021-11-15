3b4981be9f8752ffa3f300822e582d77fb82a8bf
capkeik <new.mail2018@mail.ru>
2021-11-15 08:46:24 +0300

    add bottom navigation

---
+++ app/src/main/java/com/example/androiddev2k1s/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.androiddev2k1s
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/androiddev2k1s/MainActivity.kt
+++ app/src/main/java/com/example/androiddev2k1s/MainActivity.kt
@@ -2,9 +2,27 @@ package com.example.androiddev2k1s
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.androiddev2k1s.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
