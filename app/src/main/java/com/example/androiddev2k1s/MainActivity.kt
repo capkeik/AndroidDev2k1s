38f5111e5855be03bcc0819a9b4ff0e9979e8a4b
capkeik <new.mail2018@mail.ru>
2021-11-15 04:49:33 +0300

    delete nav component

--- app/src/main/java/com/example/androiddev2k1s/MainActivity.kt
+++ app/src/main/java/com/example/androiddev2k1s/MainActivity.kt
@@ -2,13 +2,9 @@ package com.example.androiddev2k1s
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
     }
 }
